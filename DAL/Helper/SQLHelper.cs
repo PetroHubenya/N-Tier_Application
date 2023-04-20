cc09b40e9c88a9c42d60203823460dd76bc2f91c
Petro Hubenya <p.hubenya@gmail.com>
2023-04-20 18:49:45 +0200

    Add a Helper class to the DAL to interact with the database.

---
+++ DAL/Helper/SQLHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Helper
+{
+    internal class SQLHelper
+    {
+        /* Define the Connection String in your SQL Helper
+         * so that you don't need to again and again create a connection
+         * and it to your method when you interact with the database. */
+
+        public static readonly string CONN_STRING = ConfigurationManager.ConnectionStrings["Connections"].ConnectionString;  
+    }
+}
