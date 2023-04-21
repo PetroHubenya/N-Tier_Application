4412ccbb431f08c589e96b83b6be4aa6e9b5e8f4
Petro Hubenya <p.hubenya@gmail.com>
2023-04-21 14:57:14 +0200

    Update StudentDAL

--- DAL/StudentDAL.cs
+++ DAL/StudentDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL.Helper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace DAL
 {
     public class StudentDAL
     {
+        public DataSet FetchSelectedStudents()
+        {
+            string sqlCommand = "SELECT * FROM Student WHERE StudentID < 5;";
+            DataSet dataSet = SQLHelper.ExecuteDataSet(SQLHelper.CONN_STRING, CommandType.Text, sqlCommand);
+            return dataSet;
+        }
     }
 }
