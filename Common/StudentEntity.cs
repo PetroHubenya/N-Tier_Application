4cc3b425f29f6932261a1a9e00c44850dcd1c081
Petro Hubenya <p.hubenya@gmail.com>
2023-04-21 14:52:04 +0200

    Update StudentEntity

--- Common/StudentEntity.cs
+++ Common/StudentEntity.cs
@@ -8,5 +8,88 @@ namespace Common
 {
     public class StudentEntity
     {
+        int studentID;
+
+
+
+        public int StudentID
+
+        {
+
+            get { return studentID; }
+
+            set { studentID = value; }
+
+        }
+
+        string name;
+
+
+
+        public string Name
+
+        {
+
+            get { return name; }
+
+            set { name = value; }
+
+        }
+
+        string email;
+
+
+
+        public string Email
+
+        {
+
+            get { return email; }
+
+            set { email = value; }
+
+        }
+
+        string address;
+
+
+
+        public string Address
+
+        {
+
+            get { return address; }
+
+            set { address = value; }
+
+        }
+
+        int age;
+
+
+
+        public int Age
+
+        {
+
+            get { return age; }
+
+            set { age = value; }
+
+        }
+
+        string country;
+
+
+
+        public string Country
+
+        {
+
+            get { return country; }
+
+            set { country = value; }
+
+        }
     }
 }
