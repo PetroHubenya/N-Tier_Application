8bd4300ff8107e80ed0ea53422cd37513e52de35
Petro Hubenya <p.hubenya@gmail.com>
2023-04-21 14:58:34 +0200

    Update StudentDAL once again

--- DAL/StudentDAL.cs
+++ DAL/StudentDAL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,27 @@ namespace DAL
 {
     public class StudentDAL
     {
-        public DataSet FetchSelectedStudents()
+        public DataTable FetchSelectedStudents()
         {
-            string sqlCommand = "SELECT * FROM Student WHERE StudentID < 5;";
-            DataSet dataSet = SQLHelper.ExecuteDataSet(SQLHelper.CONN_STRING, CommandType.Text, sqlCommand);
-            return dataSet;
+            // string sqlCommand = "SELECT * FROM Student WHERE StudentID < 5;";
+            // DataSet dataSet = SQLHelper.ExecuteDataSet(SQLHelper.CONN_STRING, CommandType.Text, sqlCommand);
+
+            string query = "SELECT * FROM Student WHERE StudentID < 5;";
+            DataTable dataTable = new DataTable();
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SQLHelper.CONN_STRING))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    dataTable.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return dataTable;
         }
     }
 }
